2c6ae2f568a49db942801ce4915d5e2b332ec2c6
GONZALO <CMG_DEV@hotmail.com>
2024-07-28 21:18:10 -0300

    DAAaaa

--- src/routes.ts
+++ src/routes.ts
@@ -46,7 +46,7 @@ router.post("/agregarPajarito", (req: Request, res: Response) => {
     .save()
     .then(() => {
       res.send(
-        "Pajarito agregado correctamente CHOTAS MARTIN SE LA COMEEE DASDASDAS"
+        "Pajarito agregado correctamente CHOTAS MARTIN SE LA COMEEE DASDASDASaaaaaaaaaaaaaaaaaaaa"
       );
     })
     .catch((err) => {
